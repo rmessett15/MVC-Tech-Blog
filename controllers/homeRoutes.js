96ed94ce2d998deb7bdda0af54b839436331a2e9
rmessett15 <rmessett15@gmail.com>
2023-05-31 12:42:13 -0500

    fixed the dashboard page so that user data could be included and passed through using handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -76,6 +76,7 @@ router.get("/dashboard", withAuth, async (req, res) => {
       include: [
         {
           model: BlogPost,
+          include: [User],
         },
         {
           model: Comment,
@@ -84,6 +85,7 @@ router.get("/dashboard", withAuth, async (req, res) => {
     });
 
     const user = userData.get({ plain: true });
+    console.log(user)
 
     res.render("dashboard", {
       ...user,
